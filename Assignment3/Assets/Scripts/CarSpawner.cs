60230337258cfc0cf04e27b9b334371cc9e25189
Lane <lanec92@gmail.com>
2021-02-08 22:23:56 -0600

    Half Complete tutorial

---
+++ Assignment3/Assets/Scripts/CarSpawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class CarSpawner : MonoBehaviour
+{
+    public float spawnDelay = .3f;
+
+    public GameObject car;
+
+    float nextTimeToSpawn = 0f;
+
+    void Update ()
+    {
+        if (nextTimeToSpawn <= Time.time)
+        {
+            SpawnCar();
+            nextTimeToSpawn = Time.time + spawnDelay;
+        }
+    }
+
+    void SpawnCar ()
+    {
+        Instantiate(car);
+
+    }
+}
---
+++ Assignment3/Assets/Scripts/Frog.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Frog : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+   
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+            rb.MovePosition(rb.position + Vector2.right);
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+            rb.MovePosition(rb.position + Vector2.left);
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+            rb.MovePosition(rb.position + Vector2.up);
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+            rb.MovePosition(rb.position + Vector2.down);
+    }
+}
